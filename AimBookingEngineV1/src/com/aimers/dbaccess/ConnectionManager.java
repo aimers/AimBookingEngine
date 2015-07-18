1a258d24f404d1a81e347355429b054699665c26
uxdevsupport@aimersinfosoft.com <Aimers@123>
2015-07-18 09:25:30 +0530

    FIRST CHECKIN
    Change-Id: I6f020188aa0554fa9bafc4c8384ed8a726b75555
    Signed-off-by: uxdevsupport@aimersinfosoft.com <Aimers@123>

---
+++ AimBookingEngineV1/src/com/aimers/dbaccess/ConnectionManager.java
@@ -0,0 +1,58 @@
+package com.aimers.dbaccess;
+
+import java.sql.*;
+
+
+public class ConnectionManager{
+
+			public Connection con=null;
+			public Statement stm=null;
+			public ResultSet rs=null;
+
+	public Boolean Connect(String dbaccess){
+			try{
+
+				Class.forName("com.mysql.jdbc.Driver").newInstance();//sun.jdbc.odbc.JdbcOdbcDriver");
+				if(dbaccess.equals("MYSQL")){
+					//TODO: Replace hardcoding with properties parameters
+					con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306","root","sa");
+				}
+				
+				try {
+					con.createStatement().executeQuery(
+							"Select count(*) from `core`.`TEMP`");//TODO: Replace with table
+				} catch (Exception e) {
+					e.printStackTrace();
+					try {
+						Class.forName("com.mysql.jdbc.Driver").newInstance();
+					} catch (InstantiationException e1) {
+						e1.printStackTrace();
+					} catch (IllegalAccessException e1) {
+						e1.printStackTrace();
+					}
+					con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306","root","sa");
+				}
+				stm=con.createStatement();
+			}
+			catch(Exception ex){
+				return false;
+			}
+			return true;
+	}
+	public Boolean Close(String dbaccess){
+			try{
+
+				if(dbaccess.equals("MYSQL")){
+					con.close();
+					con=null;
+				}
+				stm=null;
+				rs=null;
+			}
+			catch(Exception ex){
+				return false;
+			}
+			return true;
+	}
+
+}
