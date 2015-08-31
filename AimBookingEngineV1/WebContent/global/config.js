7e8c4e29ef22b6c29d2a4c9b2606ca66b20189e5
uxdevsupport@aimersinfosoft.com <Aimers@123>
2015-08-31 22:06:23 +0530

    Vendor Entities fetch register fix
    Change-Id: I02f95c0d81a319dca8eb0f8f61d0b3c0b25f9647
    Signed-off-by: uxdevsupport@aimersinfosoft.com <Aimers@123>

--- AimBookingEngineV1/WebContent/global/config.js
+++ AimBookingEngineV1/WebContent/global/config.js
@@ -4,7 +4,7 @@ medApp.global.config = {};
 medApp.global.config.applicationMode = true;
 
 medApp.global.config.development = {
-  base : "http://localhost:8080/AimBookingEngineV1/",
+  base : "http://bookingdemo.aimersinfosoft.com/",
   endPoints : {
     vendorData : "assets/data/",
     vendorDataList : "assets/data/vendorsData.json",
@@ -17,7 +17,7 @@ medApp.global.config.development = {
 };
 
 medApp.global.config.production = {
-  base : "http://localhost:8080/AimBookingEngineV1/",
+  base : "http://bookingdemo.aimersinfosoft.com/",
   endPoints : {
     vendorData : "MasterServlet?AimAction=",
     vendorDataList : "assets/data/vendorsData.json",
